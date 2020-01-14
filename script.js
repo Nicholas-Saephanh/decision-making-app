59395fd727e52cc391665fb780ebfac8aeb99af7
Nicholas Saephanh <nicksaephanh@gmail.com>
2020-01-14 14:28:34 -0800

    who the heck knows anymore. Added dependencies for physics based features to be added later on. also some 3 js testing

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 "use strict";
+
 //===============================================
 //		APP IIFE
 //===============================================
@@ -18,7 +19,7 @@ function addListItem() {
 	let userInputText = document.getElementById("newItemText").value.trim();
 	//create html text node from userInputText
 	let newTextNode = document.createTextNode(userInputText);
-	//Get the alert box at top of list app
+	//Get the alert box at top of list appendChild
 	let randomListAlert = document.getElementById('randomListAlert');
 
 
@@ -143,3 +144,38 @@ function coinFlip() {
 	document.getElementById("coinFlip").addEventListener("click", coinFlip, false);
 	document.getElementById("chooseRandom").addEventListener("click", chooseRandomListItem, false)
 })(window, document);
+
+  // Setup your threejs scene
+    var scene = new THREE.Scene();
+    // ...
+    
+    // Setup your cannonjs world
+    var world = new CANNON.World();
+    // ...
+    
+    DiceManager.setWorld(world);
+    
+    // Create a dice
+    var dice = new DiceD6({backColor: '#ff0000'});
+    scene.add(dice.getObject());
+    
+    // If you want to place the mesh somewhere else, you have to update the body
+    dice.getObject().position.x = 150;
+    dice.getObject().position.y = 100;
+    dice.getObject().rotation.x = 20 * Math.PI / 180;
+    dice.updateBodyFromMesh();
+    
+    // Set the value of the side, which will be upside after the dice lands
+    DiceManager.prepareValues([{dice: dice, value: 6}]);
+    
+    //Animate everything
+    function animate() {
+        world.step(1.0 / 60.0);
+        
+        dice.updateMeshFromBody(); // Call this after updating the physics world for rearranging the mesh according to the body
+        
+        renderer.render(scene, camera);
+        
+        requestAnimationFrame(animate);
+    }
+    requestAnimationFrame(animate);
